267d8792e4f5951694d95297a5677024cba44405
Marcio Woitek <mwoitekjr@gmail.com>
2024-02-13 14:46:18 -0300

    EDA: Add section on PassengerId

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -4,9 +4,14 @@
 
 # %%
 from pathlib import Path
+from typing import cast
 
 import matplotlib.pyplot as plt
 import polars as pl
+import seaborn as sns
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.ticker import AutoMinorLocator
 
 # %%
 pl.Config.set_tbl_cols(14)
@@ -49,3 +54,59 @@ df_train.null_count()
 # %%
 # Test data
 df_test.null_count()
+
+# %% [markdown]
+# ## `PassengerId`
+
+# %%
+# Extract groups
+groups = df_train.get_column("PassengerId").str.split("_").list.first()
+groups.head()
+
+# %%
+# Number of unique groups
+groups.n_unique()
+
+# %%
+# Add as a DataFrame column
+df_train = df_train.with_columns((pl.col("PassengerId").str.split("_").list.first()).alias("Group"))
+df_train.head(10)
+
+# %%
+# Create a couple of features from `Group`
+df_groups = (
+    df_train.group_by("Group")
+    .len()
+    .rename({"len": "GroupSize"})
+    .with_columns(
+        [
+            (pl.col("GroupSize") - 1).alias("CompanionCount"),
+            (pl.col("GroupSize") == 1).alias("Alone"),
+        ]
+    )
+    .drop("GroupSize")
+)
+df_groups
+
+# %%
+df_train = df_train.join(df_groups, on="Group", how="left")
+df_train.head(10)
+
+# %%
+# Number of people traveling alone
+fig = plt.figure(figsize=(6.0, 6.0), layout="tight")
+fig = cast(Figure, fig)
+
+ax = fig.add_subplot()
+ax = cast(Axes, ax)
+
+sns.countplot(x=df_train.get_column("Alone").to_numpy(), order=[True, False], ax=ax)
+ax.set_title("Traveling alone?")
+ax.set_xticklabels(["Yes", "No"])
+ax.set_ylabel("Count")
+ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+
+plt.show()
+
+# %%
+df_train.get_column("Alone").value_counts(sort=True)
