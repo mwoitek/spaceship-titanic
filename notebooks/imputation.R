5066be347662ab85ecdde64ecf3a276783df82ba
Marcio Woitek <mwoitekjr@gmail.com>
2024-02-27 00:43:11 -0300

    Data imputation: Imputation with missForest

--- notebooks/imputation.R
+++ notebooks/imputation.R
@@ -42,24 +42,24 @@ df_train <- read_csv(
   show_col_types = FALSE,
   col_types = list(
     PassengerId = col_character(),
-    Alone = col_logical(),
+    Alone = col_factor(levels = c("True", "False"), ordered = FALSE),
     CompCntReduced = col_factor(levels = c("0", "1", "2", "3+"), ordered = TRUE),
     HomePlanetOrd = col_factor(levels = c("0.0", "1.0", "2.0"), ordered = FALSE),
-    CryoSleep = col_logical(),
+    CryoSleep = col_factor(levels = c("True", "False"), ordered = FALSE),
     CabinDeckOrd = col_factor(
       levels = c("0.0", "1.0", "2.0", "3.0", "4.0", "5.0"),
       ordered = FALSE
     ),
-    CabinPort = col_logical(),
+    CabinPort = col_factor(levels = c("True", "False"), ordered = FALSE),
     DestinationOrd = col_factor(levels = c("0.0", "1.0", "2.0"), ordered = FALSE),
     DiscretizedAge4 = col_factor(levels = c("0.0", "1.0", "2.0", "3.0"), ordered = TRUE),
     DiscretizedAge5 = col_factor(levels = c("0.0", "1.0", "2.0", "3.0", "4.0"), ordered = TRUE),
-    VIP = col_logical(),
-    PosRoomService = col_logical(),
-    PosFoodCourt = col_logical(),
-    PosShoppingMall = col_logical(),
-    PosSpa = col_logical(),
-    PosVRDeck = col_logical(),
+    VIP = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosRoomService = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosFoodCourt = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosShoppingMall = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosSpa = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosVRDeck = col_factor(levels = c("True", "False"), ordered = FALSE),
     PTTotalSpent = col_double(),
     Transported = col_skip()
   )
@@ -83,24 +83,24 @@ df_test <- read_csv(
   show_col_types = FALSE,
   col_types = list(
     PassengerId = col_character(),
-    Alone = col_logical(),
+    Alone = col_factor(levels = c("True", "False"), ordered = FALSE),
     CompCntReduced = col_factor(levels = c("0", "1", "2", "3+"), ordered = TRUE),
     HomePlanetOrd = col_factor(levels = c("0.0", "1.0", "2.0"), ordered = FALSE),
-    CryoSleep = col_logical(),
+    CryoSleep = col_factor(levels = c("True", "False"), ordered = FALSE),
     CabinDeckOrd = col_factor(
       levels = c("0.0", "1.0", "2.0", "3.0", "4.0", "5.0"),
       ordered = FALSE
     ),
-    CabinPort = col_logical(),
+    CabinPort = col_factor(levels = c("True", "False"), ordered = FALSE),
     DestinationOrd = col_factor(levels = c("0.0", "1.0", "2.0"), ordered = FALSE),
     DiscretizedAge4 = col_factor(levels = c("0.0", "1.0", "2.0", "3.0"), ordered = TRUE),
     DiscretizedAge5 = col_factor(levels = c("0.0", "1.0", "2.0", "3.0", "4.0"), ordered = TRUE),
-    VIP = col_logical(),
-    PosRoomService = col_logical(),
-    PosFoodCourt = col_logical(),
-    PosShoppingMall = col_logical(),
-    PosSpa = col_logical(),
-    PosVRDeck = col_logical(),
+    VIP = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosRoomService = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosFoodCourt = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosShoppingMall = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosSpa = col_factor(levels = c("True", "False"), ordered = FALSE),
+    PosVRDeck = col_factor(levels = c("True", "False"), ordered = FALSE),
     PTTotalSpent = col_double()
   )
 ) %>%
@@ -110,3 +110,54 @@ str(df_test)
 
 # %%
 head(df_test, n = 10L)
+
+# %% [markdown]
+# ## Imputation with `missForest`
+
+# %%
+# Training data
+train_imp <- missForest(
+  df_train,
+  maxiter = 10,
+  ntree = 100,
+  variablewise = TRUE,
+  verbose = TRUE
+)
+
+# %%
+head(train_imp$ximp, n = 10L)
+
+# %%
+err <- train_imp$OOBerror
+num_miss <- colSums(is.na(df_train))
+mask <- num_miss > 0
+
+tibble(
+  Feature = names(num_miss[mask]),
+  MissingValues = num_miss[mask],
+  PFC = err[mask] # PFC = Proportion of falsely classified
+) %>% column_to_rownames("Feature")
+
+# %%
+# Test data
+test_imp <- missForest(
+  df_test,
+  maxiter = 10,
+  ntree = 100,
+  variablewise = TRUE,
+  verbose = TRUE
+)
+
+# %%
+head(test_imp$ximp, n = 10L)
+
+# %%
+err <- test_imp$OOBerror
+num_miss <- colSums(is.na(df_test))
+mask <- num_miss > 0
+
+tibble(
+  Feature = names(num_miss[mask]),
+  MissingValues = num_miss[mask],
+  PFC = err[mask] # PFC = Proportion of falsely classified
+) %>% column_to_rownames("Feature")
